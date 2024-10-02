51df8cdeb2f4052df12b49b5948c240179ca2584
syedanastanweer <syedanastanwer@gmail.com>
2024-10-02 21:36:57 +0500

    commit1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+
+import React from 'react';
+import Header from './components/Header'; // Adjust the path if necessary
+
+const App = () => {
+    return (
+        <div className="App">
+            <Header />
+            {/* You can add more components here */}
+        </div>
+    );
+};
+
+export default App;
